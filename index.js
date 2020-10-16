afbb115e28a24b4a216273d95c1946054fdc14cb
Gosse <thms.gosse@gmail.com>
2020-10-16 22:32:53 +0200

    chore minor modifs

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import { AppRegistry } from 'react-native';
 import App from './src/components/App';
 import { name as appName } from './app.json';
 
+// import { DevSettings } from 'react-native';
+
+// if (__DEV__) {
+//   DevSettings._nativeModule.setHotLoadingEnabled(false);
+// }
+
 AppRegistry.registerComponent(appName, () => App);
