46d4e518b08c7b1f60a6a22f6809406119db8ebb
Gosse <thms.gosse@gmail.com>
2020-12-17 23:10:10 +0100

    feat: use env to get api url and call it to retrieve various infos

--- src/services/init-service.js
+++ src/services/init-service.js
@@ -1,8 +1,18 @@
+import axios from 'axios';
+import { API_URL } from '@env';
+
 import { getData, storeData } from '../services/local-storage';
+import data from '../data';
 
 export async function init() {
   const ingredientsVersion = await getData('ingredients-version');
   if (!ingredientsVersion) {
+    await storeData('ingredients', data);
     await storeData('ingredients-version', 1);
+  }
+
+  const result = await axios.get(`${API_URL}/getVersion`);
+  if (ingredientsVersion === result.data?.version) {
+    console.log('Alredy up to date !');
   }
 }
